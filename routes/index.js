4f746735ee816a90d96827e08ae190a29784d381
Nicolas Caceres <nicolascaceresacosta@hotmail.com>
2019-09-12 17:57:09 -0500

    Prueba

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 // Connection URL
-var url = process.env.MONGOLAB_URI;
+const url = "mongodb://nacaceres:cupitips@cluster0-shard-00-00-2gfpv.mongodb.net:27017,cluster0-shard-00-01-2gfpv.mongodb.net:27017,cluster0-shard-00-02-2gfpv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority”;
 // Database Name
 const dbName = "cupitips";
 
