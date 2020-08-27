f91b75f35518fe7975d191c96cc8162d091332be
Nicolas <n.bello@uniandes.edu.co>
2020-08-27 15:32:01 -0500

    Se configura el repositorio para la maquina virtual

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ let indexRouter = require("./routes/index");
 let usersRouter = require("./routes/users");
 
 let app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
